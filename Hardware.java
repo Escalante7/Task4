8105c29b26f43ed6f97768d3f6e175823ed0a366
Brix pogi na masarap <kyancastanar@gmail.com>
2026-03-23 22:55:10 +0800

    Update Hardware.java
    ya pa aksept ya

--- Hardware.java
+++ Hardware.java
@@ -5,3 +5,10 @@ public class Hardware {
     private String brand;
     private int spec;
     private String type;
+    
+public Hardware(int id, String brand, int spec, String type) {
+        this.id = id;
+        this.brand = brand;
+        this.spec = spec;
+        this.type = type;
+    }
