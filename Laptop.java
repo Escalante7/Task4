a29a9377d99999b8956ab43e5e00c8df827e172c
Escalante7 <chrisdavepaloma00@gmail.com>
2026-03-23 23:02:51 +0800

    Update Laptop.java

--- Laptop.java
+++ Laptop.java
@@ -3,3 +3,8 @@ public class Laptop extends Hardware {
     public Laptop(int id, String brand, int spec) {
         super(id, brand, spec, "Laptop");
     }
+     @Override
+    public String getSpecDisplay() {
+        return getSpec() + "GB RAM";
+    }
+}
