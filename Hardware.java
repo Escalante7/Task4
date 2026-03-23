80e7d60406e9b12cce15e74a321c01f671264c30
Brix pogi na masarap <kyancastanar@gmail.com>
2026-03-23 22:44:11 +0800

    Update Hardware.java
    1st commit

---
+++ Hardware.java
@@ -0,0 +1,7 @@
+// Super class - Requirement 1: Inheritance Hierarchy
+// Requirement 5: Encapsulation (all fields private)
+public class Hardware {
+    private int id;
+    private String brand;
+    private int spec;
+    private String type;
